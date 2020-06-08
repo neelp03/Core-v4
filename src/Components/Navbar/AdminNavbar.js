b388fc8ec127c4759c79964e6f4db19a5fb62655
surabhi gupta <surabhi2029@gmail.com>
2020-06-08 15:17:53 -0700

    Fixed Small Errors

--- src/Components/Navbar/AdminNavbar.js
+++ src/Components/Navbar/AdminNavbar.js
@@ -1,41 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Navbar,
   NavbarBrand,
   NavLink,
   Nav,
   Collapse,
-  NavbarToggler
-} from 'reactstrap';
-import { membershipState } from '../../Enums';
+  NavbarToggler,
+} from "reactstrap";
+import { membershipState } from "../../Enums";
 
 export default function AdminNavbar(props) {
   const [collapsed, setCollapsed] = useState(true);
   const navbarLinks = [
-    { title: 'Home', route: '/' },
-    { title: 'Overview', route: '/dashboard' },
-    { title: 'Event Manager', route: '/event-manager' },
-    { title: 'LED Sign', route: '/led-sign' },
-    { title: '3DConsole', route: '/3DConsole' }
+    { title: "Home", route: "/" },
+    { title: "Overview", route: "/dashboard" },
+    { title: "Event Manager", route: "/event-manager" },
+    { title: "LED Sign", route: "/led-sign" },
+    { title: "3DConsole", route: "/3DConsole" },
   ];
 
   return (
-    <Navbar dark className='admin-nav admin-dark navbar-expand-md' navbar>
-      <NavbarBrand href='/'>
-      Admin Dashboard
-      </NavbarBrand>
+    <Navbar
+      dark
+      className="admin-nav admin-dark navbar-expand-md"
+      navbar="true"
+    >
+      <NavbarBrand href="/">Admin Dashboard</NavbarBrand>
       <NavbarToggler
         onClick={() => setCollapsed(!collapsed)}
-        className='mr-2'
+        className="mr-2"
       />
-      <div class="menu-btn">
-        <div class="menu-btn_burger"></div>
-      </div>
-
-      <script src="AdminNavbar.js"></script>
 
       <Collapse isOpen={!collapsed} navbar>
-        <Nav className='ml-auto sce-nav' navbar>
+        <Nav className="ml-auto sce-nav" navbar>
           {navbarLinks.map((link, index) => {
             const navlink = (
               <NavLink key={index} title={link.title} href={link.route}>
@@ -47,15 +44,17 @@ export default function AdminNavbar(props) {
             // just return the link.
             return link.restricted
               ? props.user &&
-              props.user.accessLevel === membershipState.ADMIN &&
-              navlink
+                  props.user.accessLevel === membershipState.ADMIN &&
+                  navlink
               : navlink;
           })}
-          <div onClick={props.handleLogout} className='nav-button nav-link'>
-            <svg style={{ width: '18px', height: '18px' }} viewBox='0 0 24 24'>
-              <path d='M17,17.25V14H10V10H17V6.75L22.25,12L17,17.25M13,2A2,
+          <div onClick={props.handleLogout} className="nav-button nav-link">
+            <svg style={{ width: "18px", height: "18px" }} viewBox="0 0 24 24">
+              <path
+                d="M17,17.25V14H10V10H17V6.75L22.25,12L17,17.25M13,2A2,
               2 0 0,1 15,4V8H13V4H4V20H13V16H15V20A2,2 0 0,1 13,22H4A2,
-              2 0 0,1 2,20V4A2,2 0 0,1 4,2H13Z' />
+              2 0 0,1 2,20V4A2,2 0 0,1 4,2H13Z"
+              />
             </svg>
             Logout
           </div>
