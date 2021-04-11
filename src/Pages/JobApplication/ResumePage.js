830d828105d9d8ccf5257049c9baa3db3739158c
Annabel1054 <annabelkusumo@gmail.com>
2021-04-10 20:50:22 -0700

    Refactored PDF + buttons, added navbar

--- src/Pages/JobApplication/ResumePage.js
+++ src/Pages/JobApplication/ResumePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './resumePage.css';
-import { Row, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Row, Modal, ModalHeader, ModalBody, Button, ModalFooter } from 'reactstrap';
 import { Document, Page, pdfjs } from "react-pdf";
 import annabel from './annabel.pdf';
 import nanar from './nanar.pdf';
@@ -12,7 +12,7 @@ import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const headerProps = {
-    title: 'Resume Page'
+  title: 'Resume Page'
 };
 
 class ResumePage extends Component {
@@ -23,14 +23,14 @@ class ResumePage extends Component {
       PDFFile: "",
       openModal: false,
       pdfs: [
-          {
-            fileName: "Annabel.pdf",
-            file: annabel,  
-          },
-          {
-            fileName: "Nanar.pdf",
-            file: nanar,
-          }
+        {
+          fileName: "Annabel.pdf",
+          file: annabel,
+        },
+        {
+          fileName: "Nanar.pdf",
+          file: nanar,
+        }
       ]
     };
 
@@ -40,17 +40,17 @@ class ResumePage extends Component {
   }
 
   handleChange(event) {
-    this.setState({jobDescription: event.target.value});
+    this.setState({ jobDescription: event.target.value });
   }
 
-  handleToggle(name, file){
-    this.setState({fileName: name});
-    this.setState({PDFFile: file});
-    this.setState({openModal: true});
+  handleToggle(name, file) {
+    this.setState({ fileName: name });
+    this.setState({ PDFFile: file });
+    this.setState({ openModal: true });
   }
 
-  handleClose(){
-    this.setState({openModal: false});
+  handleClose() {
+    this.setState({ openModal: false });
   }
 
   fileUploadButton = () => {
@@ -67,53 +67,55 @@ class ResumePage extends Component {
       <div>
         <Header {...headerProps} />
         <div className='page-container'>
-            <ProSidebar className='side-nav'>
-                <Menu>
-                    <MenuItem>
-                        Resume Upload Page
-                        <Link to= '/UploadResumePage'/>
-                    </MenuItem>
-                    <MenuItem>
-                        Highlight Text Page
-                        <Link to= '/HighlighterPage'/>
-                    </MenuItem>
-                    <MenuItem>
-                        Resume Page
-                        <Link to= '/ResumePage'/>
-                    </MenuItem>
-                </Menu>
-            </ProSidebar>
-            <Modal size='lg' contentClassName='custom-modal' isOpen={this.state.openModal}>
-                <ModalHeader className='modal-header'>
-                    {this.state.fileName}
-                    <button className='close' onClick={this.handleClose}>Close</button> 
-                </ModalHeader>
-                <ModalBody>
-                    <Document
-                        className='pdf' 
-                        file={this.state.PDFFile}
-                        onLoadError={console.error}
-                        onLoadSuccess={this.onDocumentLoadSuccess}
-                    >   
-                        <Page pageNumber={1} />
-                    </Document>
-                </ModalBody>
-            </Modal>
-            <div className= 'resume-container'>
+          <ProSidebar className='side-nav'>
+            <Menu>
+              <MenuItem>
+                Resume Upload Page
+                        <Link to='/UploadResumePage' />
+              </MenuItem>
+              <MenuItem>
+                Highlight Text Page
+                        <Link to='/HighlighterPage' />
+              </MenuItem>
+              <MenuItem>
+                Resume Page
+                        <Link to='/ResumePage' />
+              </MenuItem>
+            </Menu>
+          </ProSidebar>
+          <Modal size='lg' contentClassName='custom-modal' isOpen={this.state.openModal}>
+            <ModalHeader className='modal-header'>
+              {this.state.fileName}
+              {/* <Button color="danger" className='close' onClick={this.handleClose}>Close</Button> */}
+            </ModalHeader>
+            <ModalBody className='pdf'>
+              <Document
+                file={this.state.PDFFile}
+                onLoadError={console.error}
+                onLoadSuccess={this.onDocumentLoadSuccess}
+              >
+                <Page className="page" pageNumber={1} />
+              </Document>
+            </ModalBody>
+            <ModalFooter>
+              <Button className='close' onClick={this.handleClose}>Close</Button>
+            </ModalFooter>
+          </Modal>
+          <div className='resume-container'>
+            <hr />
+            {this.state.pdfs.map((type, ind) => (
+              <div>
+                <Row key={ind} className='row'>
+                  <div
+                    onClick={() => this.handleToggle(type.fileName, type.file)}>
+                    {type.fileName}
+                  </div>
+                  <Button className='delete-btn'>Delete</Button>
+                </Row>
                 <hr />
-                {this.state.pdfs.map((type, ind) => (
-                <div>
-                    <Row key={ind} className='row'>
-                        <div 
-                            onClick={() => this.handleToggle(type.fileName, type.file)}>
-                                {type.fileName}
-                        </div>
-                        <button className='delete-btn'>Delete</button>
-                    </Row>
-                    <hr />
-                </div>
-                ))}
-            </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
